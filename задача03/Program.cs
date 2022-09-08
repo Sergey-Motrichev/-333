18cf81d9bef5467d433a637dc687c2f5b9d00bbb
Sergey-Motrichev <112479065+Sergey-Motrichev@users.noreply.github.com>
2022-09-08 16:31:22 +0200

    решение2.3

---
+++ задача03/Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+int day=Convert.ToInt32(Console.ReadLine());
+if(day==1){
+    System.Console.WriteLine("Рабочий-понедельник");
+}
+else if(day==2){
+    System.Console.WriteLine("Рабочий-вторник");
+}
+else if(day==3){
+    System.Console.WriteLine("Рабочая-среда");
+}
+else if(day==4){
+    System.Console.WriteLine("Рабочий-четверг");
+}
+else if(day==5){
+    System.Console.WriteLine("Рабочая-пятница");
+}
+else if(day==6){
+    System.Console.WriteLine("Выходной-суббота");
+}
+else if(day==7){
+    System.Console.WriteLine("Выходной-воскресенье");
+}
+else{
+    System.Console.WriteLine("Несуществующий день недели");
+}
