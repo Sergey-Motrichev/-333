efaebc4e71ee34e2b26c62459eeabc2d16869bf3
Sergey-Motrichev <112479065+Sergey-Motrichev@users.noreply.github.com>
2022-09-08 15:55:09 +0200

    решение2.2

---
+++ задача02/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = 32679;
+
+if(n<100){
+    System.Console.WriteLine("третьей цифры нет");
+}
+else{
+    int result= n/100%10;
+    System.Console.WriteLine($"третья цифра числа{n}->{result}");
+}
+
